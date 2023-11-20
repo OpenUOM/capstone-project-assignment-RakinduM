3cb9310387ad7b7c030456e39d941f7694f4103f
RakinduM <rakindumarambe17@gmail.com>
2023-11-21 04:05:36 +0530

    Frontend changes

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -7,57 +7,53 @@ import { environment } from './../environments/environment';
 })
 export class AppServiceService {
 
-  readonly ROOT_URL;
+    readonly ROOT_URL;
 
-  constructor(private http: HttpClient) {
-    if(environment.production == false){
-      this.ROOT_URL = 'test'
-    }else{
-      this.ROOT_URL = 'api'
+    constructor(private http: HttpClient) {
+        this.ROOT_URL = 'http://localhost:8081'
     }
-  }
 
-  initializeDB(){
-    return this.http.get(`/${this.ROOT_URL}/dbinitialize`)
-  }
+      initializeDB(){
+        return this.http.get(`/api/dbinitialize`)
+      }
 
-  getTeacherData(){
-    return this.http.get(`/${this.ROOT_URL}/listTeachers`)
-  }
+      getTeacherData() {
+            return this.http.get('/api/listTeachers')
+      }
 
-  getStudentData(){
-    return this.http.get(`/${this.ROOT_URL}/listStudents`)
-  }
+      getStudentData(){
+        return this.http.get(`/api/listStudents`)
+      }
 
-  getOneStudentData(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/getStudentInfo`, payload)
-  }
+      getOneStudentData(payload: Object){
+        return this.http.post(`/api/getStudentInfo`, payload)
+      }
 
-  getOneTeacherData(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/getTeacherInfo`, payload)
-  }
+      getOneTeacherData(payload: Object) {
+        return this.http.post('/api/getTeacherInfo', payload)
+      }
 
-  addTeacher(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/addTeacher`, payload)
-  }
+    addTeacher(payload: Object) {
+        return this.http.post('/api/addTeacher', payload)
+    }
 
-  deleteTeacher(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/deleteTeacher`, payload)
-  }
+    deleteTeacher(payload: Object) {
+        return this.http.post('/api/deleteTeacher', payload)
+    }
 
-  editTeacher(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/editTeacher`, payload)
-  }
+    editTeacher(payload: Object) {
+        return this.http.post('/api/editTeacher', payload)
+    }
 
-  editStudent(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/editStudent`, payload)
-  }
+      editStudent(payload: Object){
+        return this.http.post(`/api/editStudent`, payload)
+      }
 
-  addStudent(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/addStudent`, payload)
-  }
+      addStudent(payload: Object){
+        return this.http.post(`/api/addStudent`, payload)
+      }
 
-  deleteStudent(payload: Object){
-    return this.http.post(`/${this.ROOT_URL}/deleteStudent`, payload)
-  }
+      deleteStudent(payload: Object){
+        return this.http.post(`/api/deleteStudent`, payload)
+      }
 }
